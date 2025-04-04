b278840409c630b1c619df980937c63fe7028e2b
sreejaboddula-248 <2210030248@klh.edu.in>
2025-04-04 20:00:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const port = process.env.PORT || 3030;
 
-app.get('/', (req, res) => res.send('Hello World! prasad updated version 2'))
+app.get('/', (req, res) => res.send('Hi! I am Sreeja Boddula'))
 
 app.listen(port, (err) => {
     if (err) {
